d021297ae0d04b3adf5e868edc71d73324ff7156
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2021-07-06 21:58:34 -0300

    Some fixes & changes

--- boolify/__init__.py
+++ boolify/__init__.py
@@ -2,52 +2,87 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-DEFAULT_VALUES = ( '1', 'able', 'active', 'allow', 'allowed', 'enabled', 'in', 'ok', 'on', 'running', 't', 'true', 'up', 'y', 'yes' )
+TRUE = (
+  '1',
+  'able',
+  'active',
+  'allow',
+  'allowed',
+  'enabled',
+  'in',
+  'ok',
+  'on',
+  'running',
+  't',
+  'true',
+  'up',
+  'y',
+  'yes',
+)
+FALSE = (
+  '0',
+  'disabled',
+  'disallowed',
+  'down',
+  'f',
+  'false',
+  'off',
+  'n',
+  'no',
+  'stopped',
+  'out',
+)
+try:
+  string_types = basestring
+except NameError:
+  try:
+    from six import string_types
+  except ImportError:
+    string_types = str
 
-def boolify(raw=None, custom_values=None, *, non_str=True, raise_exc=False):
-  def _boolify(raw, custom_values=None, *, non_str=True, raise_exc=False):
-    def is_str(raw):
-      try:
-        return isinstance(raw, basestring)
-      except NameError:
-        try:
-          from six import string_types
-        except ImportError:
-          return isinstance(raw, str)
-        else:
-          return isinstance(raw, string_types)
-    if isinstance(raw, bool):
-      return raw
-    if not non_str and not is_str(raw):
-      return bool(raw)
-    try:
-      from distutils.util import strtobool
-    except ImportError:
-      pass
-    if isinstance(raw, float):
-      raw = round(raw) #int(raw)
-    raw = str(raw).strip().lower()
+try:
+  from distutils.util import strtobool
+except ImportError:
+  strtobool = None
+
+_str = lambda raw: isinstance(raw, string_types)
+
+def _boolify(raw, custom_values=None, *, non_str=True, raise_exc=False, **kw):
+  if isinstance(raw, bool):
+    return raw
+  if not non_str and not _str(raw):
+    return bool(raw)
+  if isinstance(raw, float):
+    #int(raw)
+    return _boolify(round(raw), custom_values=custom_values, non_str=non_str, raise_exc=raise_exc, **kw)
+  raw = str(raw).strip().lower().lower()
+  if raw in FALSE and raw in TRUE or custom_values is not None and raw in custom_values:
     try:
       return bool(strtobool(raw))
-    except (NameError, ValueError, AttributeError):
-      if raise_exc and not raw in (custom_values or DEFAULT_VALUES):
-        raise ValueError('Invalid value %r. Expected values are : \'%s\'' % (raw, '\', \''.join((custom_values or DEFAULT_VALUES))))
-      return raw in (custom_values or DEFAULT_VALUES)
+    except (AttributeError, NameError, TypeError, ValueError):
+      pass
+  if raise_exc and raw not in (custom_values or TRUE) and raw not in FALSE:
+    raise ValueError('Invalid value %r. Expected values are : \'%s\'' % (raw, '\', \''.join((custom_values or TRUE))))
+  return raw in (custom_values or TRUE)
+
+def boolify(raw=None, custom_values=None, *, non_str=True, raise_exc=False, **kw):
   if isinstance(raw, (list, dict, tuple, set)) and len(raw) > 0:
     if hasattr(raw, 'iteritems') or hasattr(raw, 'items'):
       return {k: _boolify(raw=_, custom_values=custom_values, non_str=non_str, raise_exc=raise_exc) for k,_ in (raw.items() if hasattr(raw, 'items') else raw.iteritems())}
     return [_boolify(raw=_, custom_values=custom_values, non_str=non_str, raise_exc=raise_exc) for _ in raw]
   return _boolify(raw=raw, custom_values=custom_values, non_str=non_str, raise_exc=raise_exc)
 
-boolify.__doc__ = """Function that will translate common strings into bool values.
+boolify.__doc__ = """
+Function that will translate common strings into bool values.
 
   Case is ignored for strings. These string values are handled:
-    True -> {DEF}
+    True -> '{TRUE}'
     False -> any other string
 
-  {PARAM3}:
+  {PARAM1}:
     Non-string values are passed to bool.
 
-  When '{PARAM3}' is passed as False, Objects other than string will be transformed using built-in bool() function.
+  When '{PARAM1}' is passed as False, Objects other than string will be transformed using built-in bool() function.
 
-  Raises ValueError when '{PARAM4}' is passed as True and it gets a string it doesn't handle.""".format(DEF='\', \''.join(DEFAULT_VALUES), PARAM3=boolify.__code__.co_varnames[-2], PARAM4=boolify.__code__.co_varnames[-1])
+  Raises ValueError when '{PARAM2}' is passed as True and it gets a string it doesn't handle.
+""".format(TRUE="', '".join(TRUE), PARAM1=boolify.__code__.co_varnames[-3], PARAM2=boolify.__code__.co_varnames[-2])
